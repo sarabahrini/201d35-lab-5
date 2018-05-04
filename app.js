45edda8a8341310c201bea86dcf14bdbcd1c49dd
Jane Smith <example@email.com>
2018-05-04 16:24:19 -0700

    Added the SumProduct function.

--- app.js
+++ app.js
@@ -46,12 +46,19 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+function sumAndMultiply(a, b, c,) { 
+    var firstSum= sum (a,b);
+    var secondSum= sum(firstSum[0],c);
+    var firstMulti=multiply(a,b);
+    var secondMulti=multiply(firstMulti[0],c);
+    var messageSum='The sum of '+a +' and '+b +'and '+'is '+secondSum;
+    console.log(messageSum);
+    var messageMulti='The product of '+a +' and '+b +'and '+'is '+secondMulti;
+    return [secondSum[0],secondMulti[0],messageSum,messageMulti];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
