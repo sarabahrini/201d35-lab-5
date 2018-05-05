d46b04f7f10327b167ebe93c6cbdff2a5117fb2c
Jane Smith <example@email.com>
2018-05-04 19:16:40 -0700

    I  adde 2 functions summultiply and sumarray.

--- app.js
+++ app.js
@@ -70,13 +70,15 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
-
+function sumArray(testArray) {
+    var result1=sum(testArray[0],testArray[1]);
+    var result=sum(result1[0],testArray[2]);
+    var secondElement=testArray[0]+','+testArray[1]+','+testArray[2]+ ' was passed in as an array of numbers, and '+ result[0]+' is their sum.';
+return[result[0],secondElement];
 }
-
 // Here is the test for sumArray(); uncomment it to run it
-
-// testSumArray(testArray);
+ 
+testSumArray(sumArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
